dad95dbd60bf30cd4c9fa74e2a4e6085f8b51acd
Zulqarnain231 <140909250+Zulqarnain231@users.noreply.github.com>
2026-02-11 04:47:05 +0000

    updarte

--- index.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const { startMonitorAgent } = require("./monitorAgent");
-const { startEvaluatorAgent } = require("./evaluatorAgent");
-
-// Start Monitor Agent
-startMonitorAgent();
-
-// Start Evaluator Agent
-startEvaluatorAgent();
